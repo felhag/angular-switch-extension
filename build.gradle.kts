5113c8f9631ac0d44f77eb7dcb54a1ee702ab258
Felix Hagemans <felixhagemans@gmail.com>
2024-12-20 10:37:56 +0100

    fixed test case, removed kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 
 plugins {
     id("java") // Java support
-    alias(libs.plugins.kotlin) // Kotlin support
     alias(libs.plugins.intelliJPlatform) // IntelliJ Platform Gradle Plugin
     alias(libs.plugins.changelog) // Gradle Changelog Plugin
     alias(libs.plugins.qodana) // Gradle Qodana Plugin
@@ -13,11 +12,6 @@ plugins {
 
 group = providers.gradleProperty("pluginGroup").get()
 version = providers.gradleProperty("pluginVersion").get()
-
-// Set the JVM language level used to build the project.
-kotlin {
-    jvmToolchain(17)
-}
 
 // Configure project's dependencies
 repositories {
--- src/test/java/live/angular/utils/ExtensionSwitcherTest.java
+++ src/test/java/live/angular/utils/ExtensionSwitcherTest.java
@@ -1,25 +1,23 @@
 package live.angular.utils;
 
-import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ExtensionSwitcherTest {
-
     final ExtensionSwitcher switcher = new ExtensionSwitcher(null);
 
     @Test
     void getExtension() {
-        MatcherAssert.assertThat(switcher.getExtension("a.component.spec.ts"), is("spec.ts"));
-        MatcherAssert.assertThat(switcher.getExtension("a.component.ts"), is("ts"));
-        MatcherAssert.assertThat(switcher.getExtension("ats"), is(""));
+        assertEquals("spec.ts", switcher.getExtension("a.component.spec.ts"));
+        assertEquals("ts", switcher.getExtension("a.component.ts"));
+        assertEquals("", switcher.getExtension("ats"));
     }
 
     @Test
     void getNameWithoutExtension() {
-        MatcherAssert.assertThat(switcher.getNameWithoutExtension("/home/a.spec.ts"), is("/home/a"));
-        MatcherAssert.assertThat(switcher.getNameWithoutExtension("a.ts"), is("a"));
-        MatcherAssert.assertThat(switcher.getNameWithoutExtension("ats"), is("ats"));
+        assertEquals("/home/a", switcher.getNameWithoutExtension("/home/a.spec.ts"));
+        assertEquals("a", switcher.getNameWithoutExtension("a.ts"));
+        assertEquals("ats", switcher.getNameWithoutExtension("ats"));
     }
 }
